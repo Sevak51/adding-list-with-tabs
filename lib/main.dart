33988f6ee0de99677fce8971faed584387859cb0
Tigran David <deulin717@gmail.com>
2019-05-25 20:00:50 +0400

    Add image, button, description

--- lib/main.dart
+++ lib/main.dart
@@ -7,16 +7,30 @@ main() {
 class MyApp extends StatelessWidget {
 
   build(context) {
-    return MaterialApp(
-     home: Scaffold(
-       appBar: AppBar(
+    return MaterialApp (
+     home: Scaffold (
+       appBar: AppBar (
          title: Text('First app'),
          backgroundColor: Colors.green,
        ),
-       body: Container(),
-
+       body: Column (
+         children: <Widget> [
+           RaisedButton (
+             child: Text('press', style: TextStyle(color: Colors.white), ),
+             color: Colors.red,
+             onPressed: (){},
+           ),
+           Card (
+             child: Column (
+               children: <Widget>[
+                 Image.asset('assets/food.jpg'),
+                 Text('Description'),
+               ],
+             )
+           ),
+         ]
+       ),
      ),
     );
-   }
-
+  }
 }
