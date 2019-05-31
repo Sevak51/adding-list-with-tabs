e0fb75a5aec3750831f6a5b62d46d3ffdaa2c99e
Tigran David <deulin717@gmail.com>
2019-05-31 11:04:22 +0400

    Add new list

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,58 @@
 import 'package:flutter/material.dart';
-
 main() {
   runApp(MyApp());
 }
-
-class MyApp extends StatelessWidget {
-
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+class _MyAppState extends State<MyApp> {
+  List<String> _products = ['Food Tester'];
   build(context) {
-    return MaterialApp (
-     home: Scaffold (
-       appBar: AppBar (
-         title: Text('First app'),
-         backgroundColor: Colors.green,
-       ),
-       body: Column (
-         children: <Widget> [
-           RaisedButton (
-             child: Text('press', style: TextStyle(color: Colors.white), ),
-             color: Colors.red,
-             onPressed: (){},
-           ),
-           Card (
-             child: Column (
-               children: <Widget>[
-                 Image.asset('assets/food.jpg'),
-                 Text('Description'),
-               ],
-             )
-           ),
-         ]
-       ),
-     ),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('first app'),
+          backgroundColor: Colors.deepPurpleAccent,
+        ),
+        body: Column(
+          children: <Widget>[
+            RaisedButton(
+              child: Text(
+                'Press',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              color: Colors.red,
+              onPressed: () {
+                setState(() {
+                  _products.add('Advanced food tester');
+                });
+              },
+            ),
+            Column(
+              children: _products
+                  .map(
+                    (element) => Card(
+                  child: Column(
+                    children: <Widget>[
+                      Image.asset('assets/food.jpg'),
+                      Text(
+                        element,
+                        style: TextStyle(color: Colors.orange),
+                      )
+                    ],
+                  ),
+                ),
+              )
+                  .toList(),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
